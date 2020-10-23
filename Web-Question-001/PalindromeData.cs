21517edae25c9cfccf03bc0f1eabbb9a874f4be7
Brad Hamilton <bhamilton1000@gmail.com>
2020-10-23 16:58:43 -0700

    Create PalindromeData.cs

---
+++ Web-Question-001/PalindromeData.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+
+namespace TestProgram
+{
+	public class TestData
+	{
+		public List<string> palList = new List<string>() {
+			"Anna",
+			"Civic",
+			"Kayak",
+			"Level",
+			"Madam",
+			"Mom",
+			"Noon",
+			"Racecar",
+			"Radar",
+			"Redder",
+			"Refer",
+			"Repaper",
+			"Rotator",
+			"Rotor",
+			"Sagas",
+			"Solos",
+			"Stats",
+			"Tenet",
+			"Wow",
+			"Don't nod",
+			"I did, did I",
+			"My gym",
+			"Red rum, sir, is murder",
+			"Step on no pets",
+			"Top spot",
+			"Was it a cat I saw",
+			"Eva, can I see bees in a cave",
+			"No lemon, no melon"
+		};
+	}
+}
